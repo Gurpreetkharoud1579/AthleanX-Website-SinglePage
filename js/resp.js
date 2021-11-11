7213d69be9b1efa4702f3bcfc1d8758f7f60eda6
Gurpreet Singh <53054296+Gurpreetkharoud1579@users.noreply.github.com>
2021-11-11 13:58:02 +0530

    v2

--- js/resp.js
+++ js/resp.js
@@ -2,10 +2,21 @@ burger = document.querySelector('.burger');
 navbar = document.querySelector('.navbar');
 navlist = document.querySelector('.nav-list');
 rightNav = document.querySelector('.right-nav');
-
+youtube = document.getElementById('youtube');
 
 burger.addEventListener('click',  ()=>{
     navlist.classList.toggle('v-bar');
     rightNav.classList.toggle('v-bar');
     navbar.classList.toggle('h-bar');
 })
+youtube.addEventListener('click',  ()=>{
+    location.href = "https://www.youtube.com/c/athleanx";
+})
+
+
+btns = document.getElementsByClassName("watchVideo");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+      location.href = "https://www.youtube.com/watch?v=G-rS3GFslts";
+    });
+}
